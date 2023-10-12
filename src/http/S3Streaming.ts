b0221c6378e0c71a5de534e37d2b9fa9b70631d4
Oleksandr Brus <alexghostalex@gmail.com>
2023-10-12 11:07:31 +0300

    fix: fix error handling

--- src/http/S3Streaming.ts
+++ src/http/S3Streaming.ts
@@ -6,7 +6,18 @@ import { S3Repository } from '#src/repository/S3Repository.js';
 import { S3ClientResolver } from '#src/resolvers/S3ClientResolver.js';
 import { S3Service } from '#src/services/s3/S3Service.js';
 
+const errorHandler = (res, e: Error) => {
+    if (e instanceof HttpError) {
+        res.status(e.getCode());
+        res.send(e.message);
+    } else {
+        res.status(HttpCodes.INTERNAL_ERROR);
+        res.send('Server internal error');
+    }
+}
+
 export default function(config: IConfig, lf: ILoggerFactory) {
+    const logger = lf();
     const client = new S3ClientResolver(config, lf).resolve();
     const repository = new S3Repository(client, lf);
     const service = new S3Service(repository, lf);
@@ -14,16 +25,15 @@ export default function(config: IConfig, lf: ILoggerFactory) {
     return async (req, res) => {
         try {
             const stream = await service.readFile(req.params.bucket, req.params.filename);
-            stream.on('error', (error: Error) => res.end(error))
+
+            stream.on('error', (error: Error) => errorHandler(res, error));
             stream.pipe(res, { end: true });
+
+            logger.info(`Proxied file "${req.params.filename}" from bucket "${req.params.bucket}"`);
         } catch(e) {
-            if (e instanceof HttpError) {
-                res.status(e.getCode());
-                res.send(e.message);
-            } else {
-                res.status(HttpCodes.INTERNAL_ERROR);
-                res.send('Server internal error');
-            }
+            errorHandler(res, e);
+
+            logger.error(`Failed to proxy file "${req.params.filename}" from bucket "${req.params.bucket}"`)
         }
     }
 }
